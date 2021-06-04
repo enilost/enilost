a8f81f64f0327e476d7787ae504d523e01a4f452
enilost <84623657+enilost@users.noreply.github.com>
2021-06-04 11:00:22 +0300

    1

---
+++ js/script.js
@@ -0,0 +1,25 @@
+function backgroundHeader() {
+    window.setTimeout('backgroundHeader()' , 7000);
+    let i = Math.round(Math.random()*4);
+    let img = '../img/bitmap_8.png';
+    let head = document.getElementById('head');
+    console.log(head);
+    if (i == 0) {
+        img = '../img/bitmap_8.png';
+    }
+    if (i == 1) {
+        img = '../img/Internet-Family.jpg';
+    }
+    if (i == 2) {
+        img = '../img/gettyimages-697375694.jpg';
+    }
+    if (i == 3) {
+        img = '../img/bitmap_19.jpg';
+    }
+    if (i == 4) {
+        img = '../img/bitmap_39.jpg';
+    }
+    head.style.backgroundImage = `url(`+img+`)`;
+    console.log(head.style.backgroundPosition);
+};
+backgroundHeader();
