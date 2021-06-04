4e6ea2bd60c51b902e4562ac7516089146c61049
enilost <84623657+enilost@users.noreply.github.com>
2021-06-04 13:51:49 +0300

    1

--- js/script.js
+++ js/script.js
@@ -1,10 +1,16 @@
+// let head = document.getElementById('head');
+// let main = document.querySelector('html');
+//     head.style.height = window.innerHeight+'px';
 function backgroundHeader() {
     window.setTimeout('backgroundHeader()' , 7000);
     let i = Math.round(Math.random()*4);
     let img = 'img/bitmap_8.png';
+    // let head = document.getElementById('head');
+    // let main = document.querySelector('html');
+    // head.style.height = main.clientHeight+'px';
     let head = document.getElementById('head');
     let main = document.querySelector('html');
-    head.style.height = main.clientHeight+'px';
+    head.style.height = window.innerHeight+'px';
     console.log(main.clientHeight);
     console.log(head);
     if (i == 0) {
